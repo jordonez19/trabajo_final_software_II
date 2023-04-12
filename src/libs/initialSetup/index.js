b37ac7a3063bd0cdc60dcfb315e518ac68c7a621
xavi <javier19pedraza@gmail.com>
2023-04-12 18:45:51 -0500

    initial set up roles

---
+++ src/libs/initialSetup/index.js
@@ -0,0 +1,20 @@
+import RolesModel from "../../models/roles";
+
+export const createRoles = async () => {
+  try {
+    const count = await RolesModel.estimatedDocumentCount();
+
+    if (count > 0) return;
+
+    const values = await Promise.all([
+      new RolesModel({ name: "user" }).save(),
+      new RolesModel({ name: "moderator" }).save(),
+      new RolesModel({ name: "admin" }).save(),
+    ]);
+
+    console.log(values);
+
+  } catch (error) {
+    console.error(error);
+  }
+};
