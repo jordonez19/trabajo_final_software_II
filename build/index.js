36c7e6f6e3238a6207f10befeb788b20d0739a9d
Xavi <javier19pedraza@gmail.com>
2023-04-17 09:40:27 -0500

    subiendo node moudles

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-require("./database/mongodb");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-require("dotenv").config();
-var port = process.env.PORT;
-_app["default"].listen(port, console.log("server is on port:", port));
