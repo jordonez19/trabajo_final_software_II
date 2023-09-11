2f6fdb75f3a9b8346e7ae99ea67aefdeaabecf37
xavi <softwarenexosdev2@gmail.com>
2023-09-11 01:03:20 -0500

    controller functions

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,9 +1,12 @@
-//---------------------------------------------------------------
-import config from "../../config";
+import config, { mailTo } from "../../config";
 import UsersModel from "../../models/user";
 import RolesModel from "../../models/roles";
 import Jwt from "jsonwebtoken";
-//---------------------------------------------------------------
+import nodemailer from "nodemailer";
+
+const email = mailTo.email
+const pass = mailTo.pass
+
 export const signup = async (req, res) => {
   const { userName, email, password, roles } = req.body;
 
@@ -27,9 +30,9 @@ export const signup = async (req, res) => {
     expiresIn: 86400,
   });
 
-  res.json({ token:token, user: newUser });
+  res.json({ token: token, user: newUser });
 };
-//---------------------------------------------------------------
+
 export const signin = async (req, res) => {
   const userFound = await UsersModel.findOne({
     email: req.body.email,
@@ -50,5 +53,119 @@ export const signin = async (req, res) => {
     expiresIn: 86400,
   });
 
-  res.json({ token: token, user: userFound});
+  res.json({ token: token, user: userFound });
+};
+
+//----------------------------------------------------------------------
+//Password reset
+//----------------------------------------------------------------------
+// Function to send a password reset email
+const sendPasswordResetEmail = async (user, resetToken) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: email,
+      pass: pass,   
+    },
+  });
+
+  const resetLink = `${config.APP_URL}/reset-password/${resetToken}`;
+  const mailOptions = {
+    from: email,
+    to: user.email,
+    subject: "Restablecer Contraseña",
+    text: `Click en el siguiente link para restablecer tu contraseña: ${resetLink}`,
+    html: `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, Helvetica, sans-serif;
+            text-align: center;
+          }
+          .container {
+            margin-top: 20px;
+          }
+          .button {
+            background-color: #007BFF;
+            color: white;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h2>Restablece tu Contraseña</h2>
+          <p>Haz clic en el botón de abajo para restablecer tu contraseña:</p>
+          <a class="button" href="${resetLink}">Restablecer Contraseña</a>
+        </div>
+      </body>
+      </html>
+    `,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+//---------------------------------------------------------------
+// Route to request a password reset
+export const requestPasswordReset = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await UsersModel.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const resetToken = Jwt.sign({ id: user._id }, config.SECRET, {
+      expiresIn: "10m", // Set the token expiration time to 10 minutes
+    });
+
+    // Store the reset token and its expiration time in the user's document in the database
+    user.resetToken = resetToken;
+    user.resetTokenExpires = Date.now() + 10 * 60 * 1000;
+    await user.save();
+
+    // Send the password reset email with the reset token
+    await sendPasswordResetEmail(user, resetToken);
+
+    res.json({ message: "Password reset instructions sent to your email" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+//---------------------------------------------------------------
+// Route to handle password reset with token
+export const resetPassword = async (req, res) => {
+  const { token, newPassword } = req.body;
+
+  try {
+    // Find the user by the token's ID and check if the token is not expired
+    const user = await UsersModel.findOne({
+      resetToken: token,
+      resetTokenExpires: { $gt: Date.now() }, // Check if the token is not expired
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid or expired reset token" });
+    }
+
+    // Update the user's password
+    user.password = await UsersModel.encryptPassword(newPassword);
+    user.resetToken = null; // Remove the reset token
+    user.resetTokenExpires = null; // Remove the reset token expiration time
+    await user.save();
+
+    res.json({ message: "Password reset successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Invalid reset token" });
+  }
 };
